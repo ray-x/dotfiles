4a25459a6d32ab82d57a410b1e8968a3d57fddf2
Ray Xu <ray.xu@deltatre.com>
2020-06-18 14:34:10 +1000

    update kitty session files

---
+++ kitty/kv.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+VIMFILENAME=""
+
+if [ "$1" != "" ]; then
+	VIMFILENAME=$(realpath "$1")
+else
+	echo "$1"
+fi
+
+echo "launch zsh -c 'nvim5 ${VIMFILENAME}'\n">${HOME}/tmp/kv.conf
+
+kitty --session ${HOME}/tmp/kv.conf  2>/dev/null
