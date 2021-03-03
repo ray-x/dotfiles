d70cd9a34946e6ef4587dd699c76b21b574ade3d
ray-x <rayx.cn@gmail.com>
2021-03-03 15:51:06 +1100

    update install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ ln -s ~/github/dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/github/dotfiles/zsh/fzffun.zsh ~/.fzffun.zsh
 ln -s ~/github/dotfiles/conf/gitconfig ~/.gitconfig
 mkdir -p ~/Library/Application\ Support/jesseduffield/lazygit
-ln -s ~/github/dotfiles/conf/lazygit.yml ~/Library/Application\ Support/jesseduffield/lazygit/config.yml
+ln -s ~/github/dotfiles/config/lazygit.yml ~/Library/Application\ Support/jesseduffield/lazygit/config.yml
 ln -s ~/github/dotfiles/nvim ~/.config/nvim
 ln -s ~/github/dotfiles/ranger ~/.config/ranger
 ln -s ~/github/dotfiles/kitty ~/.config/kitty
