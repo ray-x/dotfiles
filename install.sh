dddb3c9ca7fb9cd4a1252f2d192b3a170bc7657c
ray-x <rayx.cn@gmail.com>
2021-03-04 17:47:43 +1100

    add sqls config.yml

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 ln -s ~/github/dotfiles/zsh/zimrc ~/.zimrc
 ln -s ~/github/dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/github/dotfiles/zsh/fzffun.zsh ~/.fzffun.zsh
-ln -s ~/github/dotfiles/conf/gitconfig ~/.gitconfig
+ln -s ~/github/dotfiles/config/gitconfig ~/.gitconfig
+ln -s ~/github/dotfiles/config/sqls_config.yml ~/.config/sqls/config.yml
 mkdir -p ~/Library/Application\ Support/jesseduffield/lazygit
 ln -s ~/github/dotfiles/config/lazygit.yml ~/Library/Application\ Support/jesseduffield/lazygit/config.yml
 ln -s ~/github/dotfiles/nvim ~/.config/nvim
 ln -s ~/github/dotfiles/ranger ~/.config/ranger
 ln -s ~/github/dotfiles/kitty ~/.config/kitty
 ln -s ~/github/dotfiles/nvim/vimrc ~/.vimrc
-
-
