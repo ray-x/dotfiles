76168c681dccb39184a98179153df92486683a34
ray-x <rayx.cn@gmail.com>
2023-11-13 22:57:38 +1100

    fish config

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,7 @@ set os (uname)
 #
 set -gx  LOCALSTACK_API_KEY	5idHUizXpE
 # never and ever
-
+set -U fish_user_paths /usr/bin $fish_user_paths
 
 # Environment variables - https://fishshell.com/docs/current/cmds/set.html
 set -gx EDITOR 'nvim'
@@ -42,12 +42,12 @@ set -gx GOPATH $HOME/go $HOME/projects
 # set -gx GOROOT "/usr/local/opt/go/libexec" # install with brew
 
 # set -gx GOROOT "/usr/local/go"   # install with brew pkg download from golang.org
-set -gx GOROOT "/usr/lib/go"
+
 set -gx GOHOME $HOME/go
 set -gx GOBIN $HOME/go/bin
 set -gx GO111MODULE "on"
 
-set -gx PATH ~/bin /usr/local/bin $HOME/.local/bin $PATH
+set -gx PATH ~/bin /usr/local/bin $HOME/.local/bin $HOME/.cargo/bin $PATH
 fish_add_path /usr/local/opt/curl/bin
 set -gx PATH $GOPATH/bin $PATH
 set -gx PATH $GOROOT/bin $PATH
@@ -68,25 +68,68 @@ set -gx GOPRIVATE "bitbucket.org/*,frankieone.dev/*,github.com/frankieone/*"
 
 set -gx PATH $HOME/.deno/bin $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin $HOME/.cargo/bin $PATH
 set -gx NVM_DIR "$HOME/.nvm"
+set -gx BAT_THEME "TwoDark"
 # # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 alias gvim='/usr/local/bin/mvim'
-alias nv='/usr/local/bin/nvim'
-alias vi='/usr/local/bin/nvim'
-alias vdiff='/usr/local/bin/nvim -d -u ~/github/dotfiles/init.min'
 alias gdv='git difftool --tool gvimdiff'
 alias nd='git difftool --tool nvimdiff'
-alias ls='exa'
-alias ll='exa -alh'
-alias rtl='exa -alh -s mod'
-alias tree='exa --tree'
+
+
 alias icat "kitty +kitten icat"
 alias cat='bat -p'
 alias RM='/bin/rm'
 alias gtest='richgo test ./... -count=1 -p=1'
-set -gx PATH /usr/local/opt/python@3.9/bin $PATH
+# set -gx PATH /usr/local/opt/python@3.9/bin $PATH
 set -gx PATH /usr/local/opt/llvm/bin $HOME/github/dotfiles/bin/ $PATH
-set -gx PATH /home/ray/.local/share/gem/ruby/3.0.0/bin $PATH
+set -gx PATH $HOME/.local/share/gem/ruby/3.0.0/bin $PATH
+
+if string match --quiet 'MSYS*' $os; or string match --quiet 'MINGW*' $os
+  # echo win
+  alias ls='lsd'
+  alias ll='lsd -l'
+  alias la='lsd -a'
+  alias tree='lsd --tree'
+  alias rtl='lsd -rtl'
+  alias python='/mingw64/bin/python.exe'
+  alias nv='/c/Program\ Files/Neovim/bin/nvim.exe'
+  set -gx WINHOME /c/users/rayxu
+  alias code=$WINHOME'/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code'
+  # alias pip='/usr/bin/pip'
+  set -gx GOROOT /mingw64/lib/go
+  set -gx PATH /usr/bin /bin /mingw64/bin /mingw64/sbin /mingw64/lib PATH  $PATH
+
+  if status is-interactive
+    # starship.exe init fish | source
+    # echo 'starship disabled'
+  end
+else
+  if status is-interactive
+    starship init fish | source
+  end
+  alias ls='eza'
+  alias ll='eza -alh'
+  alias rtl='eza -alh -s mod'
+  alias tree='eza --tree'
+  alias rm='trash-put'
+  alias nv='/usr/local/bin/nvim'
+  alias vi='/usr/local/bin/nvim'
+  alias vdiff='/usr/local/bin/nvim -d -u ~/github/dotfiles/init.min'
+  if status is-login
+    ssh-add
+  end
+end
+
+function start_ssh_agent
+    if not set -q SSH_AGENT_PID
+        eval (ssh-agent -c > /dev/null)
+        ssh-add ~/.ssh/id_ed25519 > /dev/null 2>&1
+    end
+end
+
+start_ssh_agent
+
+
 #
 # # Completion for kitty
 # kitty + complete setup fish | source. #fish earlier than 3.0.0,
@@ -102,7 +145,6 @@ alias fzb='fzf --preview "bat {} --color=always"'
 # alias seperator="viu `echo ~/photos/$(ls ~/photos |sort -R |tail -1)`'
 
 alias seperator='viu (echo $HOME"/photos/"(ls ~/photos |sort -R |tail -1))'
-alias rm='trash-put'
 
 set -gx JAR $HOME/lsp_test/jdt-language-server-1.3.0-202108171748/plugins/# # eclipse.equinox.launcher_1.6.100.v20201223-0822.jar
 set -gx GRADLE_HOME $HOME/gradle
@@ -115,8 +157,8 @@ if test $os = 'Linux'
 else
   set -gx HOST_NAME RayMac
   set -gx JAVA_HOME /usr/local/Cellar/openjdk/17.0.1/
-  set -gx JDTLS_HOME $HOME/lsp_test/jdt-language-server-1.3.0-202108171748
-  set -gx JDTLS_CONFIG $JDTLS_HOME/config_mac
+  # set -gx JDTLS_HOME $HOME/lsp_test/jdt-language-server-1.3.0-202108171748
+  # set -gx JDTLS_CONFIG $JDTLS_HOME/config_mac
 end
 
 set -gx WORKSPACE $HOME/workspace
@@ -138,10 +180,8 @@ if status is-interactive
   # source $XDG_CONFIG_HOME/fish/abbreviations.fish
 
   # https://github.com/starship/starship#fish
-  starship init fish | source
+  # starship init fish | source #doese not works with mingw
   zoxide init fish | source
-  # z.lua
-  # lua $HOME/bin/z --init fish | source
 
   # https://asdf-vm.com/#/core-manage-asdf-vm?id=add-to-your-shell
   # source ~/.asdf/asdf.fish
@@ -158,18 +198,16 @@ if status is-interactive
 end
 fish_add_path /usr/local/opt/ruby/bin
 fish_add_path /usr/local/lib/ruby/gems/3.0.0/bin
-if status is-login
-  ssh-add
-end
 
 alias cmdhis "_fzf_search_history"
 # pnpm
-set -gx PNPM_HOME "/home/ray/.local/share/pnpm"
+set -gx PNPM_HOME $HOME/.local/share/pnpm
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
 #
 set -gx NPM_TOKEN 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
 set -x PATH "$HOME/github/dotfiles/fish/plugins/git-fuzzy/bin:$PATH"
+set -x PATH "$PATH:/c/ProgramData/chocolatey/bin:$USERPROFILE/scoop/shims"
 set sponge_purge_only_on_exit true
 
 function rgn -d "grep preview and edit with nvim"
@@ -180,3 +218,13 @@ end
 function fdn -d "fd preview and edit with nvim"
   fd --full-path $argv | fzf --delimiter=':' --preview 'bat --color=always {1} --line-range {2}:+20' | cut -d ':' -f1 | xargs -r nvim
 end
+
+
+#function fish_prompt
+#    # Set the colors for different prompt elements
+#    set_color cyan
+#    set -g fish_color_cwd blue
+#
+#    # Build the prompt string
+#    echo -n (prompt_pwd) '❯ '
+#end
